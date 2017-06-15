52c660dae3460e6ff3016508192bc4829dc86e8a
Alec Lanter <kintar1900@gmail.com>
2017-06-15 13:27:28 -0500

    Initial commit

---
+++ ConsulClient/ConsulProvider.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsulClient.DataTypes;
+
+namespace ConsulClient
+{
+    public class ConsulProvider : IConsulProvider
+    {
+        public string ConsulHost { get; } = "127.0.0.1";
+        public int ConsulHttpPort { get; } = 8500;
+        public string ServiceHostName { get; }
+
+        public ConsulProvider()
+        {
+            ServiceHostName = Environment.MachineName;
+        }
+
+        public ConsulProvider(string host, int port, string serviceHostName)
+        {
+            ConsulHost = host;
+            ConsulHttpPort = port;
+            ServiceHostName = serviceHostName;
+        }
+
+        public bool RegisterService(ServiceRegistrationInfo service)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeregisterService(string serviceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Service> GetServices(string serviceName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RegisterCheck(CheckRegistrationInfo check)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeregisterCheck(string checkId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Check> GetChecks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateTTLCheck(string checkId, string note, ServiceCheckStatus status)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsulClient/IConsulProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using ConsulClient.DataTypes;
+
+namespace ConsulClient
+{
+    public interface IConsulProvider
+    {
+        bool RegisterService(ServiceRegistrationInfo service);
+        bool DeregisterService(string serviceId);
+        List<Service> GetServices(string serviceName);
+        bool RegisterCheck(CheckRegistrationInfo check);
+        bool DeregisterCheck(string checkId);
+        List<Check> GetChecks();
+        bool UpdateTTLCheck(string checkId, string note, ServiceCheckStatus status);
+    }
+}
